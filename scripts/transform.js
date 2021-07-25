ac34fddb00e13a290b08fe6c372b75eca1db99fe
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-07-25 18:39:15 -0500

    remove scv methods and cleanup

--- scripts/transform.js
+++ scripts/transform.js
@@ -1,7 +1,6 @@
 // functions for transforming photo.csv data with missing quotation marks
 
 const fs = require('fs');
-const csv = require('csv-parser');
 
 let chunksCount = 0;
 let fileBuffer = Buffer.alloc(1024);
@@ -22,7 +21,7 @@ const closeFd = (fd) => {
   fs.close(fd, (err) => {
     if (err) throw err;
   });
-}
+};
 
 fs.createReadStream(inputCSVPath)
   .once('error', (err) => {
@@ -45,38 +44,7 @@ fs.createReadStream(inputCSVPath)
       throw err;
     }
   });
-    // console.log('this is a transformed chunk: ', transformedChunk);
-    // fileBuffer.concat(transformedChunk);
-    // chunks.push(transformedChunk);
   })
   .on('end', function () {
     console.log('end of transformation, chunks processed:', chunksCount);
-    // let outputText = chunks.join();
-    // console.table(users)
-    // TODO: SAVE users data to another file
-    // fs.writeFile(outputCSVPath, outputText, (err) => {
-    //   if (err) {
-    //     return console.error(err);
-    //   }
-    //   console.log('transformed csv file saved successfully!');
-    // });
   });
-
-function writeToCSVFile(users) {
-  const filename = 'output.csv';
-  fs.writeFile(filename, extractAsCSV(users), err => {
-    if (err) {
-      console.log('Error writing to csv file', err);
-    } else {
-      console.log(`saved as ${filename}`);
-    }
-  });
-}
-
-function extractAsCSV(users) {
-  // const header = ["Username, Password, Roles"];
-  // const rows = users.map(user =>
-  //    `${user.username}, ${user.password}, ${user.roles}`
-  // );
-  // return header.concat(rows).join("\n");
-}
