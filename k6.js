d7b319972942e3737001d6889e6f865254746b24
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-08-07 00:45:39 -0500

    initial k6 setup

---
+++ k6.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  // http.get('https://test.k6.io');
+  // sleep(1);
+  // server test:
+  // products request:
+  // product request:
+  // styles request:
+  // related request:
+}
