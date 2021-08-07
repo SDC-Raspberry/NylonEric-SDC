0373ffa8de075466261fcbe30825b891a205dfd3
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-08-07 14:51:44 -0500

    progress on k6 load tests

--- database/index.js
+++ database/index.js
@@ -74,20 +74,20 @@ const getStyles = (product_id) => {
     console.time('styles2');
     console.time('styles3');
     return Promise.all([
-      client.query(stylesQueryString, [product_id]) //.then(result => {
-        //console.timeEnd('styles1');
-        //return result;
-      //})
+      client.query(stylesQueryString, [product_id]).then(result => {
+        console.timeEnd('styles1');
+        return result;
+      })
       ,
-      client.query(photosQueryString, [product_id]) //.then(result => {
-        //console.timeEnd('styles2');
-        //return result;
-      //})
+      client.query(photosQueryString, [product_id]).then(result => {
+        console.timeEnd('styles2');
+        return result;
+      })
       ,
-      client.query(skusQueryString, [product_id]) //.then(result => {
-        //console.timeEnd('styles3');
-        //return result;
-      //})
+      client.query(skusQueryString, [product_id]).then(result => {
+        console.timeEnd('styles3');
+        return result;
+      })
     ])
     .then(results => {
       let tempObject = {};
--- k6.js
+++ k6.js
@@ -1,12 +1,30 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const PORT = __ENV.PORT || 4321;
+
+export let options = {
+  // setupTimeout: '30s',
+  // teardownTimeout: '30s',
+  vus: 1,
+  duration: '1m',
+  // iterations: 10,
+};
+
 export default function () {
+  // console.log(`here is my counter: ${__ITER}`);
   // http.get('https://test.k6.io');
   // sleep(1);
   // server test:
+  // http.get(`http://127.0.0.1:4321/`);
+  // let currentProduct = __ITER + 1000;
+  // http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}`);
+  // http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}/styles`);
+  http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}/related`);
+  // console.log('Response time was ' + String(res.timings.duration) + ' ms');
+  sleep(1);
   // products request:
   // product request:
   // styles request:
   // related request:
-}
+};
