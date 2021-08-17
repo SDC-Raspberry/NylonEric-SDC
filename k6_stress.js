874f23e663b4385b8daf38144e44eb688953a025
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-08-17 00:10:02 -0500

    progress on k6 stress test

--- k6_stress.js
+++ k6_stress.js
@@ -7,18 +7,22 @@ const baseURL = `http://${HOST}:${PORT}/`;
 
 export let options = {
   stages: [
-    { duration: '2m', target: 100 }, // below normal load
+    { duration: '30s', target: 100 }, // below normal load
     { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 243 }, // around the breaking point
+    { duration: '30s', target: 243 }, // light load
     { duration: '5m', target: 243 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
+    { duration: '30s', target: 500 },
+    { duration: '5m', target: 500 },
+    { duration: '30s', target: 1000 },
+    { duration: '5m', target: 1000 },
+    { duration: '30s', target: 2250 }, // around the breaking point
+    { duration: '5m', target: 2250 },
+    { duration: '30s', target: 5000 }, // past the breaking point
+    { duration: '5m', target: 5000 },
+    { duration: '1m', target: 0 }, // scale down. Recovery stage.
   ],
   thresholds: {
-    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 1.5s
   },
 };
 
@@ -26,11 +30,11 @@ let res;
 
 export default function () {
   let responses = http.batch([
-    ['GET',
-      `https://test-api.k6.io//public/crocodiles/1/`,
-      null,
-      { tags: { name: 'PublicCrocs' } }
-    ],
+    // ['GET',
+    //   `https://test-api.k6.io//public/crocodiles/1/`,
+    //   null,
+    //   { tags: { name: 'PublicCrocs' } }
+    // ],
     ['GET',
       `${baseURL}`,
        null,
@@ -40,23 +44,23 @@ export default function () {
       `${baseURL}products`,
       null,
       {},
+    ],
+    ['GET',
+      `${baseURL}products/${__ITER + 900001}`,
+      null,
+      {},
+    ],
+    ['GET',
+      `${baseURL}products/${__ITER + 900001}/styles`,
+      null,
+      {},
+    ],
+    ['GET',
+      `${baseURL}products/${__ITER + 900001}/related`,
+      null,
+      {},
     ]
   ]);
-  // server test:
-  // http.get(`http://127.0.0.1:4321/`);
-
-  // products request:
-  // res = http.get(`http://127.0.0.1:4321/products`);
-
-  // product request:
-  // // let currentProduct = __ITER + 1000;
-  // let res = http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}`);
-
-  // styles request:
-  // res = http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}/styles`);
-
-  // related request:
-  // res = http.get(`http://127.0.0.1:4321/products/${__ITER + 900001}/related`);
 
 //   if (
 //     !check(res, {'status code MUST be 200': (res) => res.status == 200,
@@ -65,5 +69,5 @@ export default function () {
 //     fail('status code was *not* 200');
 //   }
   // console.log('Response time was ' + String(res.timings.duration) + ' ms');
-  sleep(1);
+  sleep(.5);
 };
