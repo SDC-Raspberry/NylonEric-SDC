a004c97794ecf65bf6bc6c0a02bb1dbdf4e45b26
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-08-21 01:13:49 -0500

    redis cache implemented for all product API endpoints

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const redis = require('redis');
 const bodyParser = require('body-parser');
 const productsMock = require('./mockData').products;
 const productMock = productsMock[0];
@@ -7,9 +8,14 @@ const relatedMock = require('./mockData').related;
 const db = require("../database/index.js");
 
 const app = express();
+const client = redis.createClient(6379);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ strict: false }));
+
+client.on('error', (err) => {
+  console.log('Redis Caching Error: ', err);
+});
 
 app.get('/', (req, res) => {
   res.status(200);
@@ -20,53 +26,111 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
   let page = Number(req.query.page || 1);
   let count = Number(req.query.count || 5);
-  db.getProducts(page, count)
-    .then(data => {
-      res.status(200).send(JSON.stringify(data));
-    })
-    .catch(error => {
-    console.error(error);
-    res.sendStatus(500);
-    });
+
+  const productsRedisKey = `/products?page=${page}&count=${count}`;
+  return client.get(productsRedisKey, (err, products) => {
+    if (products) {
+      console.log('products query result exists in cache');
+      // return res.json({
+      //   source: 'cache',
+      //   data: JSON.parse(products)
+      // });
+      return res.status(200).send(products);
+    } else {
+      db.getProducts(page, count)
+      .then(data => {
+        let productsResponse = JSON.stringify(data);
+        // save response to Redis store:
+        client.setex(productsRedisKey, 3600, productsResponse);
+        return res.status(200).send(productsResponse);
+      })
+      .catch(error => {
+        console.error(error);
+        res.sendStatus(500);
+      });
+    }
+  })
 });
 
 // GET /products/:product_id
 app.get('/products/:product_id', (req, res) => {
   let product_id = Number(req.params.product_id);
-  db.getProduct(product_id)
-  .then(data => {
-    res.status(200).send(JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error(error);
-    res.sendStatus(500);
+
+  const productRedisKey = `/products/${product_id}`;
+
+  return client.get(productRedisKey, (err, product) => {
+    if (product) {
+      console.log('product description query result exists in cache');
+      return res.status(200).send(product);
+    } else {
+      db.getProduct(product_id)
+      .then(data => {
+        let productResponse = JSON.stringify(data)
+
+        // save response to Redis store:
+        client.setex(productRedisKey, 3600, productResponse);
+        return res.status(200).send(productResponse);
+      })
+      .catch(error => {
+        console.error(error);
+        res.sendStatus(500);
+      });
+    }
   });
 });
 
 // GET /products/:product_id/styles
 app.get('/products/:product_id/styles', (req, res) => {
   let product_id = Number(req.params.product_id);
-  db.getStyles(product_id)
-  .then(data => {
-    res.status(200).send(JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error(error);
-    res.sendStatus(500);
+
+  const stylesRedisKey = `/products/${product_id}/styles`;
+
+  return client.get(stylesRedisKey, (err, styles) => {
+    if (styles) {
+      console.log('product styles query result exists in cache');
+      return res.status(200).send(styles);
+    } else {
+      db.getStyles(product_id)
+      .then(data => {
+        let stylesResponse = JSON.stringify(data);
+
+        // save response to Redis store:
+        client.setex(stylesRedisKey, 3600, stylesResponse);
+        res.status(200).send(stylesResponse);
+      })
+      .catch(error => {
+        console.error(error);
+        res.sendStatus(500);
+      });
+    }
   });
 });
 
 // GET /products/:product_id/related
 app.get('/products/:product_id/related', (req, res) => {
   let product_id = Number(req.params.product_id);
-  db.getRelated(product_id)
-  .then(data => {
-    res.status(200).send(JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error(error);
-    res.sendStatus(500);
-  })
+
+  const relatedRedisKey = `/products/${product_id}/related`;
+
+  return client.get(relatedRedisKey, (err, related) => {
+    if (related) {
+      console.log('related products query result exists in cache');
+      return res.status(200).send(related);
+    } else {
+      db.getRelated(product_id)
+      .then(data => {
+        let relatedResponse = JSON.stringify(data)
+
+        // save response to Redis store:
+        client.setex(relatedRedisKey, 3600, relatedResponse);
+        return res.status(200).send(relatedResponse);
+      })
+      .catch(error => {
+        console.error(error);
+        res.sendStatus(500);
+      });
+    }
+  });
 });
 
 module.exports = app;
